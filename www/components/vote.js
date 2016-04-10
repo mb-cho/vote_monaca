178d5ac152fd24aa7df8107b740278c5530d6e9c
mb-cho <aoki.m.f@gmail.com>
2016-04-10 14:46:57 +0900

    second commit

--- www/components/vote.js
+++ www/components/vote.js
@@ -86,7 +86,8 @@ $('#select > option:selected').remove();
 $(function(){
     //ここから始まる
     $(document).ready(function(){
-//localStorage.clear();
+localStorage.clear();
+//TEST
         var user = localStorage.getItem("user");
         if(user == null){//user が無い　＝　初回起動
             var s_url = 'https://net-navi.cc/vote/users/user_entry';
