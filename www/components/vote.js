476f088226dc15a7ca942d72ac6c2ca27a466c26
mb-cho <aoki.m.f@gmail.com>
2016-04-11 09:52:01 +0900

    Roll Back TEST

--- www/components/vote.js
+++ www/components/vote.js
@@ -87,8 +87,6 @@ $(function(){
     //ここから始まる
     $(document).ready(function(){
 //localStorage.clear();
-//2nd
-//3rd
         var user = localStorage.getItem("user");
         if(user == null){//user が無い　＝　初回起動
             var s_url = 'https://net-navi.cc/vote/users/user_entry';
